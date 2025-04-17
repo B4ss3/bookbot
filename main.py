4331ab55a6b718d643388fe417f47ef25e15ab52
B4ss3 <Vladislav.smelko97@gmail.com>
2025-04-17 14:10:16 +0300

    Programm working

---
+++ main.py
@@ -0,0 +1,37 @@
+from stats import count_words, count_characters, sort_dictionary
+import sys
+def main():
+ 
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    chars_dict = count_characters(text)
+    sorted_dict = sort_dictionary(chars_dict)
+    print_report(book_path,word_count, sorted_dict)
+
+
+
+def print_report(book_path,word_count,sorted_dict):
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}")
+    print(f"----------- Word Count ----------\nFound {word_count} total words")
+    print("--------- Character Count -------")
+    for char in sorted_dict:
+        if char.isalpha() == True:
+            print (f"{char}: {sorted_dict[char]}")
+    print("============= END ===============")
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+    
+
+main()
+
+
+
---
+++ stats.py
@@ -0,0 +1,20 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    lowered = text.lower()
+    char_dict = {}
+
+    for char in lowered:
+        if char in char_dict:
+            char_dict[char] = char_dict[char] + 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def sort_dictionary(dct):
+
+    sorting_dict = sorted(dct.items(), key=lambda kv: kv[1], reverse= True)
+    return dict(sorting_dict)
+
